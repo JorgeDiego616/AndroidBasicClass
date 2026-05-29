31afb924fb696cc2e6284e3830d22f0ca1fedd2b
Jorge <jorge.chaparronune@outlook.com>
2026-05-29 16:59:07 -0600

    Se agrego bien el boton para ver los perfiles

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -15,8 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidBasicsClassTheme {
-                LoginView()
-                /* AppNavigation() */
+                /*LoginView()*/
+                AppNavigation()
             }
         }
     }
--- app/src/main/java/com/example/emptyactivity/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/emptyactivity/ui/navigation/AppNavigation.kt
@@ -1,6 +1,6 @@
 package com.example.emptyactivity.ui.navigation
 
-
+import com.example.emptyactivity.ui.thirdpartialids2.firstAPIRequest.view.FirstApiRequestView
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
@@ -37,6 +37,8 @@ import com.example.emptyactivity.ui.thirdpartialpdm1.homeThirdPartialPDM1.view.H
  * @property icon Icon displayed in the NavigationBar item.
  */
 sealed class AppRoute(val route: String, val label: String, val icon: ImageVector) {
+    // --
+    object FirstApiRequest : AppRoute("first_api_request", "API", Icons.Filled.School)
     object ThirdPartialIDS2 : AppRoute("third_partial_ids2", "IDS2 P3", Icons.Filled.School)
     object FirstPartialPDM1 : AppRoute("first_partial_pdm1", "PDM1 P1", Icons.Filled.PhoneAndroid)
     object SecondPartialPDM1 : AppRoute("second_partial_pdm1", "PDM1 P2", Icons.Outlined.PhoneAndroid)
@@ -90,11 +92,20 @@ fun AppNavigation() {
             startDestination = AppRoute.ThirdPartialIDS2.route,
             modifier = Modifier.padding(innerPadding)
         ) {
-            composable(AppRoute.ThirdPartialIDS2.route) { HomeThirdPartialIDS2View() }
+            composable(AppRoute.ThirdPartialIDS2.route) {
+                HomeThirdPartialIDS2View(
+                    onNavigateToFirstApi = { navController.navigate("first_api_request") }
+                )
+            }
+            /*composable(AppRoute.ThirdPartialIDS2.route) { HomeThirdPartialIDS2View() }*/
             composable(AppRoute.FirstPartialPDM1.route) { HomeFirstPartialPDM1View() }
             composable(AppRoute.SecondPartialPDM1.route) { HomeSecondPartialPDM1View() }
             composable(AppRoute.ThirdPartialPDM1.route) { HomeThirdPartialPDM1View() }
             composable(AppRoute.PersonalInformation.route) { HomePersonalInformationView() }
+
+            composable("first_api_request") {
+                FirstApiRequestView(onBack = { navController.popBackStack() })
+            }
         }
     }
 }
